60d684df26a16f673c59a81bed4188c778f16e2f
Joey Iglesias <joeyiglesias83@gmail.com>
2025-06-02 08:35:45 -0400

    Configures project for testing and routing.
    Sets up Jest with ts-jest, including coverage and module path mappings.
    Configures Prettier with adjusted print width and object wrapping.
    Adds a basic router redirect based on authentication status.
    Adds testing libraries to the project

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
-import { Redirect } from 'expo-router';
-import { useContext } from 'react'
+import { Redirect } from 'expo-router'
+import React, { useContext } from 'react'
 import { AuthContext } from '@/context/auth/AuthContext'
 
 export default function Index() {
-	const {state: authState} = useContext(AuthContext)
+	const { state: authState } = useContext(AuthContext)
 
 	if (authState.token) {
 		return <Redirect href="/(tabs)" />
-	}
-	else {
-		return <Redirect href="/login" />;
+	} else {
+		return <Redirect href="/login" />
 	}
 }
--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,12 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.json');
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
 
 module.exports = {
-	preset: 'ts-jest/presets/js-with-babel', // 👈 enables Babel for ts/tsx
+	preset: 'ts-jest/presets/js-with-babel',
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 	collectCoverage: true,
-	collectCoverageFrom: [
-		'**/*.{ts,tsx}',
-		'!**/*.d.ts',
-		'!**/node_modules/**',
-		'!**/coverage/**',
-		'!**/__tests__/**',
-		'!**/app/**',
-	],
+	collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/node_modules/**', '!**/coverage/**', '!**/__tests__/**', '!**/app/**/_layout.tsx'],
 	coverageDirectory: 'coverage',
 	coverageReporters: ['text', 'html', 'lcov'],
 	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
@@ -22,4 +15,4 @@ module.exports = {
 	transform: {
 		'^.+\\.(ts|tsx)$': 'babel-jest', // 👈 override to use Babel for JSX/TSX
 	},
-};
+}
